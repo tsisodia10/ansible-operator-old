74447ba92176bcbf875520b6cba6cc1b9b131cda
Fabien Dupont <fdupont@redhat.com>
2020-06-24 05:11:32 -0400

    Propose changes to sample.sh

--- build/sample.sh
+++ build/sample.sh
@@ -1,68 +1,87 @@
-set -e
+#!/usr/bin/env bash
 
-#Check extravars and print 
+# The container image is derived from ansible-runner container image,
+# so we also extend the image entrypoint script to add some tests.
+
+# In OpenShift, containers are run as a random high number uid
+# that doesn't exist in /etc/passwd, but Ansible module utils
+# require a named user. So if we're in OpenShift, we need to make
+# one before Ansible runs.
+if [ `id -u` -ge 500 ] || [ -z "${CURRENT_UID}" ]; then
+
+  cat << EOF > /tmp/passwd
+root:x:0:0:root:/root:/bin/bash
+runner:x:`id -u`:`id -g`:,,,:/runner:/bin/bash
+EOF
+
+  cat /tmp/passwd > /etc/passwd
+  rm /tmp/passwd
+fi
+
+# The RUNNER_PLAYBOOK variable is mandatory
+if [ -z "RUNNER_PLAYBOOK" ] ; then
+    echo "ERROR - RUNNER_PLAYBOOK not specified. Exiting."
+    exit 1
+fi
+
+# We only support Git repository for project import. If the project type is
+# not specified, we set it to 'local'.
+if [ ! "${PROJECT_TYPE}" ]; then
+    echo "WARNING - PROJECT_TYPE not specified. Defaulting to 'local'."
+    PROJECT_TYPE="local"
+fi
+
+# Collect extra vars from environment and add them to /runner/env/extravars.
+# The file is created if it has not been mounted at runtime.
 [[ ! -f /runner/env/extravars ]] && echo "---" > /runner/env/extravars
 env | grep '^EXTRAVAR_' | while read EV ; do
-    EV_KEY=$(echo "$EV" | awk -F '=' '{ print $1; }' | sed 's/^EXTRAVAR_//'
-    EV_VALUE=$(echo "$EV" | awk -F '=' '{ print $2; }'
+    EV_KEY=$(echo "$EV" | awk -F '=' '{ print $1; }' | sed 's/^EXTRAVAR_//')
+    EV_VALUE=$(echo "$EV" | awk -F '=' '{ print $2; }')
     echo "${EV_KEY}: ${EV_VALUE}" >> /runner/env/extravars
 done
 
+# Collect environment variabless from environment and add them to
+# /runner/env/envvars. The file is created if it has not been mounted at
+# runtime.
 [[ ! -f /runner/env/ennvars ]] && echo "---" > /runner/env/envvars
 env | grep '^ENVVAR_' | while read ENV ; do
-    ENV_KEY=$(echo "$ENV" | awk -F '=' '{ print $1; }' | sed 's/^ENVVAR_//'
-    ENV_VALUE=$(echo "$EV" | awk -F '=' '{ print $2; }'
+    ENV_KEY=$(echo "$ENV" | awk -F '=' '{ print $1; }' | sed 's/^ENVVAR_//')
+    ENV_VALUE=$(echo "$EV" | awk -F '=' '{ print $2; }')
     echo "${ENV_KEY}: ${ENV_VALUE}" >> /runner/env/extravars
 done
 
-[[ ! -f /runner/env/extravars ]] && echo "---" > /runner/env/extravars
-env | grep '^EXTRAVAR_' | while read EV ; do
-    EV_KEY=$(echo "$EV" | awk -F '=' '{ print $1; }' | sed 's/^E_//'
-    EV_VALUE=$(echo "$EV" | awk -F '=' '{ print $2; }'
-    echo "${EV_KEY}: ${EV_VALUE}" >> /runner/env/extravars
-done
-
-
-if [! -f /runner/inventory/hosts ];
-     then echo "ERROR: Inventory is not mounted"
-     exit 1;
-
-if [! -f /runner/env/passwords && -f /runner/env/ssh_key ];
-     then exit 1;
-
-echo "Cloning GIT repository..............."
-
-if [! -z {{PROJECT_TYPE}}];
-      then echo "ERROR: Project type not specified"
-      exit 1;
-
-if [! -z {{PROJECT_URI}}];
-      then echo "ERROR: Project URI not specified"
-      exit 1;
-      
-if [! ! -d .git ] ;
-      then echo "ERROR: This isn't a git directory" && return false;
+# Check if the inventory file is present. If not, we create an empty inventory
+# with only localhost
+if [! -f /runner/inventory/hosts ]; then
+    echo "WARNING - Inventory file is absent. Creating one with localhost."
+    echo -e "[all]\nlocalhost" > /runner/inventory/hosts
 fi
 
-git_url=`git config --get remote.origin.url`
-  if [[ $git_url != git@github.com:* && $git_url != git@gitlab.com:* ]]; then
-    echo "ERROR: Remote origin is invalid" && return false;
+# Check if the passwords or SSH private key have been provided. If none is
+# present, we raise a warning, but continue as the SSH private could be shipped
+# in the container image or overridden by the playbook/roles.
+if [ ! -f /runner/env/passwords && ! -f /runner/env/ssh_key ]; then
+    echo "WARNING - Both passwords and SSH private key file are absent."
 fi
 
-url=${git_url%.git};
+if [ "${PROJECT_TYPE}" == "git" ]; then
+    if [ -z ${PROJECT_URI} ]; then
+        echo "ERROR - PROJECT_URI not specified. It is mandatory for 'git'."
+        exit 1
+    fi
 
-if [[ $url == git@github.com:* ]]; then
-    url=$(echo $url | sed 's,git@github.com:,https://github.com/,g');
-  elif [[ $url == git@gitlab.com:* ]]; then
-    url=$(echo $url | sed 's,git@gitlab.com:,https://gitlab.com/,g');
+    echo "INFO - Cloning Git repository: ${PROJECT_URI}"
+    git clone ${PROJECT_URI} /runner/project
+    if [ $? != 0 ] ; then
+        echo "ERROR - Clone of ${PROJECT_URI} failed. Exiting."
+        exit 1
+    fi
 fi
-open $url;
 
-git clone $url; > /runner/project
-echo "Git repository cloned"
+# Check if the specified playbook exists in /runner/project.
+if [ ! -f /runner/project/${RUNNER_PLAYBOOK} ]; then
+    echo "ERROR - Playbook /runner/project/${RUNNER_PLAYBOOK} does not exist."
+    exit 1
+fi
 
-if [ -f /runner/project/runner_playbook ];
-    then echo "ERROR: No playbooks"
-	    exit 1;
-
-exec ansible-runner -- "{$@}"	    
+exec tini -- "${@}"
